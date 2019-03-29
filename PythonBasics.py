9c66f22ade3cbf7e5b88133c146d9d11a09cf552
Cory <Coryf65@gmail.com>
2019-03-28 19:31:09 -0500

    don't have to declare types for variables?

--- PythonBasics.py
+++ PythonBasics.py
@@ -5,3 +5,10 @@ last_name = "Lovelace"
 print("Hello," + first_name + " ")
 print(first_name + " " + last_name + " is an awesome person!")
 
+# print() automatically adds spaces
+print("These", "will be joined", "together by spaces!")
+
+# Python will save vars like JS kinda
+is_int = 12
+
+print(is_int)
