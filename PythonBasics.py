254d04b7601ca0e7910df5f377e4f1b9fe2f13a8
Cory <Coryf65@gmail.com>
2019-03-28 19:26:06 -0500

    Python uses ( snake_casing ) Ada Lovelace is awesome!

--- PythonBasics.py
+++ PythonBasics.py
@@ -1,1 +1,7 @@
-print("Hello World")
+#Python uses Snake Case for naming conventions
+first_name = "Ada" 
+last_name = "Lovelace"
+
+print("Hello," + first_name + " ")
+print(first_name + " " + last_name + " is an awesome person!")
+
