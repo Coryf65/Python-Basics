6f7ffc9442aa13e1d7c0b6c124e23ffbf0916973
Cory <Coryf65@gmail.com>
2019-12-03 21:04:45 -0600

    while loop

---
+++ loops.py
@@ -0,0 +1,8 @@
+# password checker loop!
+
+password = input("Please enter a super secret password:  ")
+
+while password != 'open me':
+    password = input("Invalid Password please try again:  ")
+
+print("Welcome to secret town")
