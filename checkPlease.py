0baacf1ec925f4f04f68168954dad35bec620983
Cory <Coryf65@gmail.com>
2019-12-03 20:51:10 -0600

    added exception handling

--- checkPlease.py
+++ checkPlease.py
@@ -1,5 +1,7 @@
 import math
 
+# now doing exception handling
+
 # spit the cost into x amount of peeps
 def split_check(total, num_of_people):
     return math.ceil(total / num_of_people)
@@ -9,10 +11,13 @@ def split_check(total, num_of_people):
 #amount_due = split_check(84.97, 4)
 
 # now taking user input
-total_due = float(input("What is the total?  "))
-number_of_people = int(input("How many people?  "))
 
-amount_due = split_check(total_due, number_of_people)
+try:
+    total_due = float(input("What is the total?  "))
+    number_of_people = int(input("How many people?  "))
+except ValueError:
+    print("Oh nooo That's not a Valid Value, try again")
+else:
+    amount_due = split_check(total_due, number_of_people)
+    print("Each person owes ${}".format(amount_due))
 
-# display
-print("Each person owes ${}".format(amount_due))
