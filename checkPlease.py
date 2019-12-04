137281a192133f4675695c0f87bdfedcc3c43d37
Cory <Coryf65@gmail.com>
2019-12-03 20:40:42 -0600

    code cleanup

--- checkPlease.py
+++ checkPlease.py
@@ -2,8 +2,8 @@ import math
 
 # spit the cost into x amount of peeps
 def split_check(total, num_of_people):
-    cost_per_person = math.ceil(total / num_of_people)
-    return cost_per_person
+    return math.ceil(total / num_of_people)
+
 
 # store function into amount due
 #amount_due = split_check(84.97, 4)
