094233d86550d7303ee7ebcacb152a184a1e5ca7
Cory <Coryf65@gmail.com>
2019-12-03 20:16:09 -0600

    showing what code smell is !

---
+++ notifications.py
@@ -0,0 +1,15 @@
+# code smell tut
+
+#showing Praise
+praise = "You are doing great"
+praise = praise.upper()
+number_of_chars = len(praise)
+result = praise + "!" * number_of_chars
+print(result)
+
+# showing advice
+advice = "Don't forget to ask for help"
+advice = advice.upper()
+number_of_chars = len(advice)
+result = advice + "!" * number_of_chars
+print(result)
