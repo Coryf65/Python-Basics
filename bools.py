dc85211e235eeb310bf4ae88c1b0ee2f07515058
Cory <Coryf65@gmail.com>
2019-12-03 18:39:42 -0600

    more practice runs

---
+++ bools.py
@@ -0,0 +1,9 @@
+#bools in python
+bool(1)
+    #True
+
+bool("Burrito")
+    # any object that isnt empty = True
+
+bool(42)
+    #= True only 0 would be False
---
+++ ifElse.py
@@ -0,0 +1,23 @@
+# If Elses in Python
+
+first_name = input("What is your first name?  ")
+
+print("Hello", first_name)
+
+# if else branch
+if first_name == "Cory":
+    # if true
+    print("Hey me..")
+    print(first_name, "is learning Python !")
+elif first_name == "Maximiliane":
+    print(first_name, "is learning with others in the community! me too!")
+else:
+    print("{} try learning Python it's fun!".format(first_name))
+
+print("Have a great Day {}!".format(first_name))
+
+
+# = is assignment
+
+# == is comparing
+
