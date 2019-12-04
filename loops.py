44de1cf97bfaa9ed088307584621d28a24cd59ea
Cory <Coryf65@gmail.com>
2019-12-03 21:09:52 -0600

    added a Constant var

--- loops.py
+++ loops.py
@@ -1,8 +1,15 @@
+import sys
+
 # password checker loop!
-
+MASTER_PASSWORD = 'open me'
 password = input("Please enter a super secret password:  ")
+attempt_count = 1
 
-while password != 'open me':
+while password != MASTER_PASSWORD:
+
+    if attempt_count > 3:
+        exit("Too many Invalid Password attempts")
     password = input("Invalid Password please try again:  ")
+    attempt_count += 1   
 
 print("Welcome to secret town")
